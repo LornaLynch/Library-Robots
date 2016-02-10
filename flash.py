0e594742a74eea408ae3b92d03baadcdcaecfc9b
Lorna Lynch <LornaLynch@users.noreply.github.com>
2016-02-10 13:59:32 +0000

    Update flash.py

--- flash.py
+++ flash.py
@@ -1,14 +1,17 @@
 # Make your robot's antenna flash on and off a number of times. 
 
+# Import time and GPIO commands
 import time
 import RPi.GPIO as GPIO
 
+# Set the GPIO numbering mode
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
+#Set pin 17 up as an output so it can power the LED
 GPIO.setup(17,GPIO.OUT)
 
-#while True: 
+# Set the LED to flash on and off five times  
 for x in range (0,5):
   print("Beep!")
   GPIO.output(17,GPIO.HIGH)
@@ -16,8 +19,8 @@ for x in range (0,5):
   GPIO.output(17,GPIO.LOW)
   time.sleep(1)
   
-# If you want the LED to flash repeatedly, replace the following line
+# If you want the LED to flash repeatedly, replace the line of code setting the number of flashes
 # for x in range (0,5):
 # with the line below
 # while True:  
-# Press Ctrl + C to quit 
+# The LED will now continue to flash; press Ctrl + C to quit 
